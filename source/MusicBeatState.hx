2aa914e3c442e9fcc1365b1a01ff1136623d712c
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-05-09 09:17:07 +0800

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -4,6 +4,7 @@ import haxe.io.Path;
 import flixel.addons.transition.FlxTransitionableState;
 import flixel.addons.ui.FlxUIState;
 import flixel.FlxG;
+import flixel.FlxState‎;
 import openfl.media.Sound;
 import openfl.ui.Mouse;
 import openfl.ui.MouseCursor;
